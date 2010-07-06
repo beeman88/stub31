9ae48558f44895e7540b54de9a32c8733f081e5a
beeman88 <bryan.belyk@sage.com>
2010-07-06 13:54:08 -0700

    removed authentication test, now done by entering specific user names in simply (unauthenticated, unauthorized, unsubscribed)

--- curl/curl_stub.bat
+++ curl/curl_stub.bat
@@ -36,7 +36,6 @@ IF "%1"=="8081" GOTO NOLINK
 IF "%1"=="8083" GOTO NOLINK
 IF "%1"=="8084" GOTO NOLINK
 IF "%1"=="8090" GOTO LINK2
-IF "%1"=="8095" GOTO 8095
 IF "%1"=="8097" GOTO NOLINK
 IF "%1"=="8099" GOTO NOLINK
 IF "%1"=="8105" GOTO LINK2
@@ -111,31 +110,6 @@ curl -v -u%3:%4 -X POST -d @C:\Python31\%1\link_post_request_1.xml -H "Content-T
 
 REM create sync request
 curl -v -u%3:%4 -X POST -d @C:\Python31\%1\sync_digest.xml -H "Content-Type: application/atom+xml; charset=utf-8" "http://localhost:%1/sdata/billingboss/crmErp/%5/%6/$syncSource?trackingID=%2&runName=customers&runStamp=2010-10-14T08:51:02&%syncSelect%"
-
-REM sync request in progress
-curl -v -u%3:%4 http://localhost:%1/sdata/billingboss/crmErp/%5/%6/$syncSource('%2')
-
-REM sync feed
-curl -v -u%3:%4 http://localhost:%1/sdata/billingboss/crmErp/%5/%6/$syncSource('%2')
-
-REM delete sync request
-curl -v -u%3:%4 -X DELETE http://localhost:%1/sdata/billingboss/crmErp/%5/%6/$syncSource('%2')
-
-GOTO EOF
-
-:8095
-
-REM authorization request OK
-curl -v -u%3:%4 http://localhost:%1/sdata/billingboss/bb/-/users('email%sp%eq%sp%%3')
-
-REM get all customers
-curl -v -u%3:%4 http://localhost:%1/sdata/billingboss/crmErp/%5/%6?%select%
-
-REM post customer new links
-curl -v -u%3:%4 -X POST http://localhost:%1/sdata/billingboss/crmErp/%5/%6/$linked
-
-REM create sync request
-curl -v -u%3:%4 -X POST http://localhost:%1/sdata/billingboss/crmErp/%5/%6/$syncSource?trackingID=%2&runName=%6&runStamp=2010-10-14T08:51:02
 
 REM sync request in progress
 curl -v -u%3:%4 http://localhost:%1/sdata/billingboss/crmErp/%5/%6/$syncSource('%2')
