28c2a8ce49f0882e884a6ea47e05a3e815b957b1
beeman88 <bryan.belyk@sage.com>
2010-07-06 13:54:52 -0700

    added stub_tests.bat to run a sequence of tests

--- stub_tests.bat
+++ stub_tests.bat
@@ -1,4 +1,17 @@
 call start_stub 8080 y
+call start_stub 8081 
+call start_stub 8082
+call start_stub 8083
+call start_stub 8084
+call start_stub 8090
 call start_stub 8500
 call start_stub 9050
+call start_stub 9070
+call start_stub 9075
+call start_stub 9080
+call start_stub 9085
+call start_stub 9090
 call start_stub 9500
+call start_stub 9600
+call start_stub 9700
+
